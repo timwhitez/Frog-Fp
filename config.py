9a4ae63b30af8b139213c58db9271f7dd3772c01
TimWhite <36320909+timwhitez@users.noreply.github.com>
2020-12-09 16:00:25 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ proxies = {
 recursion = 2
 
 #目录fuzz超时(s)
-fuzztime = 5
+fuzztime = 3
 
 #指纹识别超时(s)
 timeout = 5
@@ -17,19 +17,22 @@ timeout = 5
 crawler_timeout = 1000
 
 #指纹识别线程数
-threads = 100
+threads = 200
 
 #目录fuzz线程数
 fuzz_t = 20
 
 #web端口扫描超时
-hb_time = 5
+hb_time = 3
 
 #端口扫描线程数
-hb_t = 200
+hb_t = 500
 
 #端口扫描端口list
 ports = "80,443,8080,8443"
 
 #仅基础扫描
-only_basic = True
+only_basic = False
+
+#深度扫描进行爬虫
+deep_crawl = False
